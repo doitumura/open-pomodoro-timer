b6577c1c3748df6b837010638d3545142745fcc9
doitumura <1993nakayama@gmail.com>
2023-02-28 10:29:34 +0900

    PomodoroTimerクラスを切り出し

--- index.js
+++ index.js
@@ -1,65 +1,6 @@
-const sleep = (ms) => {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-const readline = require("readline");
-const printRemainingTime = (targetMinutes) => {
-  return new Promise(async (resolve) => {
-    const targetSeconds = targetMinutes * 60;
-    let elapsedSeconds = 0;
-    while(elapsedSeconds < targetSeconds) {
-      readline.clearLine(process.stdout);
-      const remainingMinutes = String(Math.floor((targetSeconds - elapsedSeconds) / 60)).padStart(2, '0');
-      const remainingSeconds = String((targetSeconds - elapsedSeconds) % 60).padStart(2, '0');
-      process.stdout.write(`\r${remainingMinutes}:${remainingSeconds}`);
-
-      await sleep(1000);
-      elapsedSeconds++;
-    }
-    resolve();
-  })
-}
-
-const open = require("open");
-const openAppAndWebpage = async (timerType) => {
-  if (timerType == "focus") process.stdout.write("\r\x1b[31mfocus");
-  if (timerType == "break") process.stdout.write("\r\x1b[32mbreak");
-  if (timerType == "longbreak") process.stdout.write("\r\x1b[32mlongbreak");
-
-  open(`page/html/${timerType}.html`);
-  await sleep(3000);
-
-  if(settingsJson[`${timerType}App`]) await open.openApp(settingsJson[`${timerType}App`]);
-  if(settingsJson[`${timerType}Url`]) await open(settingsJson[`${timerType}Url`]);
-  await sleep(1000);
-  open.openApp("Terminal");
-}
-
-const startFocusTimer = async (settingsJson, cycle) => {
-  await openAppAndWebpage("focus");
-  await printRemainingTime(settingsJson["focus"]);
-
-  cycle++
-  if(settingsJson["cycle"] == 0 || cycle != settingsJson["cycle"]) {
-    startBreakTimer(settingsJson, cycle);
-  } else {
-    startLongbreakTimer(settingsJson);
-  }
-}
-
-const startBreakTimer = async (settingsJson, cycle) => {
-  await openAppAndWebpage("break");
-  await printRemainingTime(settingsJson["break"]);
-  startFocusTimer(settingsJson, cycle);
-}
-
-const startLongbreakTimer = async (settingsJson) => {
-  await openAppAndWebpage("longbreak");
-  await printRemainingTime(settingsJson["longbreak"]);
-  startFocusTimer(settingsJson, 0);
-}
-
 const fs = require("fs");
 const settingsJson = JSON.parse(fs.readFileSync("settings.json", "utf8"));
 
-startFocusTimer(settingsJson, 0);
+const PomodoroTimer = require("./pomodoro-timer.js");
+const pomodoroTimer = new PomodoroTimer(settingsJson);
+pomodoroTimer.start();
---
+++ pomodoro-timer.js
@@ -0,0 +1,71 @@
+class PomodoroTimer {
+  constructor(settingsJson) {
+    this.settingsJson = settingsJson;
+  }
+
+  start = () => {
+    this.#startFocusTimer(0);
+  }
+
+  #startFocusTimer = async (cycle) => {
+    await this.#openAppAndWebpage("focus");
+    await this.#printRemainingTime(this.settingsJson["focus"]);
+
+    cycle++
+    if(this.settingsJson["cycle"] == 0 || cycle != this.settingsJson["cycle"]) {
+      this.#startBreakTimer(cycle);
+    } else {
+      this.#startLongbreakTimer();
+    }
+  }
+
+  #startBreakTimer = async (cycle) => {
+    await this.#openAppAndWebpage("break");
+    await this.#printRemainingTime(this.settingsJson["break"]);
+    this.#startFocusTimer(cycle);
+  }
+
+  #startLongbreakTimer = async () => {
+    await this.#openAppAndWebpage("longbreak");
+    await this.#printRemainingTime(this.settingsJson["longbreak"]);
+    this.#startFocusTimer(0);
+  }
+
+  #printRemainingTime = (targetMinutes) => {
+    return new Promise(async (resolve) => {
+      const targetSeconds = targetMinutes * 60;
+      let elapsedSeconds = 0;
+      const readline = require("readline");
+
+      while(elapsedSeconds < targetSeconds) {
+        readline.clearLine(process.stdout);
+        const remainingMinutes = String(Math.floor((targetSeconds - elapsedSeconds) / 60)).padStart(2, '0');
+        const remainingSeconds = String((targetSeconds - elapsedSeconds) % 60).padStart(2, '0');
+        process.stdout.write(`\r${remainingMinutes}:${remainingSeconds}`);
+  
+        await this.#sleep(1000);
+        elapsedSeconds++;
+      }
+      resolve();
+    })
+  }
+
+  #openAppAndWebpage = async (timerType) => {
+    if (timerType == "focus") process.stdout.write("\r\x1b[31mfocus");
+    if (timerType == "break") process.stdout.write("\r\x1b[32mbreak");
+    if (timerType == "longbreak") process.stdout.write("\r\x1b[32mlongbreak");
+  
+    const open = require("open");
+    open(`page/html/${timerType}.html`);
+    await this.#sleep(3000);
+  
+    if(this.settingsJson[`${timerType}App`]) await open.openApp(this.settingsJson[`${timerType}App`]);
+    if(this.settingsJson[`${timerType}Url`]) await open(this.settingsJson[`${timerType}Url`]);
+    await this.#sleep(1000);
+    open.openApp("Terminal");
+  }
+
+  #sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+}
+
+module.exports = PomodoroTimer;
