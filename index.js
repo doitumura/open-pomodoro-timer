717b470c65db80d99e47d9bbe27a7ad78783b2de
doitumura <1993nakayama@gmail.com>
2023-03-01 02:44:02 +0900

    設定を変更するサブコマンドを追加

--- index.js
+++ index.js
@@ -16,9 +16,51 @@ const DEFAULT_SETTINGS = {
   "browser":"Google Chrome"
 }
 
+const overwriteSettings = async (option, abbreviatedOption) => {
+  let settingsJson = await JSON.parse(fs.readFileSync(SETTINGS_SAVE_PATH, "utf8"));
+  settingsJson[option] = argv[option] ? argv[option] : argv[abbreviatedOption];
+  fs.writeFileSync(SETTINGS_SAVE_PATH, JSON.stringify(settingsJson), "utf8");
+}
+
 const argv = require("minimist")(process.argv.slice(2));
 if(argv["_"][0] == "default") {
   fs.writeFileSync(SETTINGS_SAVE_PATH, JSON.stringify(DEFAULT_SETTINGS), "utf8");
+}
+
+if(argv["_"][0] == "set") {
+  if(argv["focus"] || argv["f"]) {
+    overwriteSettings("focus", "f");
+  }
+  if(argv["break"] || argv["b"]) {
+    overwriteSettings("break", "b");
+  }
+  if(argv["longbreak"] || argv["lb"]) {
+    overwriteSettings("longbreak", "lb");
+  }
+  if(argv["cycle"] || argv["c"]) {
+    overwriteSettings("cycle", "c");
+  }
+  if(argv["focusApp"] || argv["fa"]) {
+    overwriteSettings("focusApp", "fa");
+  }
+  if(argv["focusUrl"] || argv["fu"]) {
+    overwriteSettings("focusUrl", "fu");
+  }
+  if(argv["breakApp"] || argv["ba"]) {
+    overwriteSettings("breakApp", "ba");
+  }
+  if(argv["breakUrl"] || argv["bu"]) {
+    overwriteSettings("breakUrl", "bu");
+  }
+  if(argv["longbreakUrl"] || argv["lu"]) {
+    overwriteSettings("longbreakUrl", "lu");
+  }
+  if(argv["longbreakApp"] || argv["la"]) {
+    overwriteSettings("longbreakApp", "la");
+  }
+  if(argv["browser"] || argv["br"]) {
+    overwriteSettings("browser", "br");
+  }
 }
 
 if(argv["_"][0] == "settings") {
