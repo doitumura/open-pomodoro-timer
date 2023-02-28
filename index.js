83b04c1b53aafc66ebb6161e4f3299cd2b9f61ad
doitumura <1993nakayama@gmail.com>
2023-03-01 06:04:12 +0900

    Dispatcherクラスを削除

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const Dispatcher = require("./dispatcher.js");
-const dispatcher = new Dispatcher();
-dispatcher.dispatch();
+const argv = require("minimist")(process.argv.slice(2));
+const subCommand = argv["_"][0];
+
+const Setting = require('./setting.js');
+const setting = new Setting(argv);
+
+switch(subCommand) {
+  case "default":
+    setting.restoreDefaultValue();
+    break;
+  case "set":
+    setting.setOptionValue();
+    break;
+  case "settings":
+    setting.print();
+    break;
+  case "start":
+    const PomodoroTimer = require("./pomodoro-timer.js");
+    const pomodoroTimer = new PomodoroTimer(setting.settingsJson);
+    pomodoroTimer.start();
+    break;
+  default:
+    console.log("no subcommand entered")
+}
