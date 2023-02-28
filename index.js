094782855d9861c3207e63437e4e5ad0d1f89c19
doitumura <1993nakayama@gmail.com>
2023-03-01 02:18:19 +0900

    サブコマンドを追加

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 const fs = require("fs");
 
-let settingsJson;
-if(fs.existsSync("settings.json")) {
-  settingsJson = JSON.parse(fs.readFileSync("settings.json", "utf8"));
-} else {
-  settingsJson = {
-    "focus":25,
-    "break":5,
-    "longbreak":15,
-    "cycle":4,
-    "focusApp":"",
-    "focusUrl":"",
-    "breakApp":"",
-    "breakUrl":"",
-    "longbreakApp":"",
-    "longbreakUrl":"",
-    "browser":"Google Chrome"
-  }
-  fs.writeFileSync("settings.json", JSON.stringify(settingsJson), "utf8");
+const SETTINGS_SAVE_PATH = "settings.json";
+
+const DEFAULT_SETTINGS = {
+  "focus":25,
+  "break":5,
+  "longbreak":15,
+  "cycle":4,
+  "focusApp":"",
+  "focusUrl":"",
+  "breakApp":"",
+  "breakUrl":"",
+  "longbreakApp":"",
+  "longbreakUrl":"",
+  "browser":"Google Chrome"
 }
 
-const PomodoroTimer = require("./pomodoro-timer.js");
-const pomodoroTimer = new PomodoroTimer(settingsJson);
-pomodoroTimer.start();
+const argv = require("minimist")(process.argv.slice(2));
+if(argv["_"][0] == "default") {
+  fs.writeFileSync(SETTINGS_SAVE_PATH, JSON.stringify(DEFAULT_SETTINGS), "utf8");
+}
+
+if(argv["_"][0] == "settings") {
+  if(fs.existsSync(SETTINGS_SAVE_PATH)) {
+    console.log(JSON.parse(fs.readFileSync(SETTINGS_SAVE_PATH, "utf8")));
+  } else {
+    console.log(DEFAULT_SETTINGS);
+  }
+}
+
+if(argv["_"][0] == "start") {
+  let settingsJson;
+  if(fs.existsSync(SETTINGS_SAVE_PATH)) {
+    settingsJson = JSON.parse(fs.readFileSync(SETTINGS_SAVE_PATH, "utf8"));
+  } else {
+    settingsJson = DEFAULT_SETTINGS;
+    fs.writeFileSync(SETTINGS_SAVE_PATH, JSON.stringify(settingsJson), "utf8");
+  }
+
+  const PomodoroTimer = require("./pomodoro-timer.js");
+  const pomodoroTimer = new PomodoroTimer(settingsJson);
+  pomodoroTimer.start();
+}
+
