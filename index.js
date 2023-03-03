24dcb90e46594311b91a7ce3832cc442f16a2fc8
doitumura <1993nakayama@gmail.com>
2023-03-03 14:22:49 +0900

    eslintとprettierを導入

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const argv = require("minimist")(process.argv.slice(2));
 const subCommand = argv["_"][0];
-if(!subCommand) {
+if (!subCommand) {
   console.error("enter subcommand");
-  return;
-}
+} else {
+  delete argv._;
+  const option = argv;
 
-delete argv._;
-const option = argv;
-const Setting = require('./lib/setting.js');
-const setting = new Setting(option);
+  const Setting = require("./lib/setting.js");
+  const setting = new Setting(option);
 
-switch(subCommand) {
-  case "set":
-    setting.setOptionValue();
-    break;
-  case "settings":
-    setting.print();
-    break;
-  case "start":
-    const PomodoroTimer = require("./lib/pomodoro-timer.js");
-    const pomodoroTimer = new PomodoroTimer(setting.settingsJson);
-    pomodoroTimer.start();
-    break;
-  default:
-    console.error("invalid subcommand");
+  const PomodoroTimer = require("./lib/pomodoro-timer.js");
+  const pomodoroTimer = new PomodoroTimer(setting.settingsJson);
+
+  switch (subCommand) {
+    case "set":
+      setting.setOptionValue();
+      break;
+    case "settings":
+      setting.print();
+      break;
+    case "start":
+      pomodoroTimer.start();
+      break;
+    default:
+      console.error("invalid subcommand");
+  }
 }
