e72534ec5da0fb681f8213344a23d40ffb6a4d44
doitumura <1993nakayama@gmail.com>
2023-03-04 10:43:30 +0900

    サブコマンドの存在判定をスイッチ文で実行するように変更

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const argv = require("minimist")(process.argv.slice(2));
 const subCommand = argv["_"][0];
-if (!subCommand) {
-  console.error("enter subcommand");
-} else {
-  delete argv._;
-  const option = argv;
+delete argv._;
+const option = argv;
 
-  const Setting = require("./lib/setting.js");
-  const setting = new Setting(option);
+const Setting = require("./lib/setting.js");
+const setting = new Setting(option);
+const PomodoroTimer = require("./lib/pomodoro-timer.js");
+const pomodoroTimer = new PomodoroTimer(setting.settingsJson);
 
-  const PomodoroTimer = require("./lib/pomodoro-timer.js");
-  const pomodoroTimer = new PomodoroTimer(setting.settingsJson);
-
-  switch (subCommand) {
-    case "set":
-      setting.setOptionValue();
-      break;
-    case "settings":
-      setting.print();
-      break;
-    case "start":
-      pomodoroTimer.start();
-      break;
-    default:
-      console.error("invalid subcommand");
-  }
+switch (subCommand) {
+  case undefined:
+    console.error("enter subcommand");
+    break;
+  case "set":
+    setting.setOptionValue();
+    break;
+  case "settings":
+    setting.print();
+    break;
+  case "start":
+    pomodoroTimer.start();
+    break;
+  default:
+    console.error("invalid subcommand");
 }
